7de1d7fefa1fc6545321c3f6eb3c9ca3da6175ed
Victor3294 <vblum26@gmail.com>
2024-04-27 17:58:29 -0300

    feat: 🎸 <CadastroUsuario>Implementação do viaCEP
    Implementação do api através da função buscaCep()

--- src/pages/CadastroUsuario/index.jsx
+++ src/pages/CadastroUsuario/index.jsx
@@ -1,6 +1,21 @@
 import { useForm } from "react-hook-form";
 function CadastroUsuario() {
-  const { register } = useForm();
+  const { register, setValue, getValues } = useForm();
+  const buscaCep = () =>{
+    let cep = getValues("cep")
+    if(!!cep && cep.length == 9){
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => response.json())
+        .then((dados) => {
+            setValue("logradouro", dados.logradouro);
+            setValue("complemento", dados.complemento);
+            setValue("bairro", dados.bairro);
+            setValue("cidade", dados.localidade);
+            setValue("uf", dados.uf);
+        })
+        .catch((error) => console.log(error))
+    }
+  }
   return (
     <>
       <h1>Cadastro Usuario</h1>
@@ -33,65 +48,83 @@ function CadastroUsuario() {
           <label htmlFor="">CPF</label>
           <input
             type="text"
+            placeholder="Digite seu CPF"
             {...register("cpf", {
               required: "Por favor informe seu CPF",
             })}
           />
         </div>
         <div>
-            <label htmlFor="">Data de Nascimento</label>
-            <input type="date" 
+          <label htmlFor="">Data de Nascimento</label>
+          <input
+            type="date"
             {...register("data-de-nascimento", {
-                required: "Por favor informe sua data de nascimento"
+              required: "Por favor informe sua data de nascimento",
             })}
-            />    
-        </div>    
-        <div>
-            <label htmlFor="">Email</label>
-            <input type="email" {...register("email", {
-                required: "Por favor informe seu email"
-            })} />
+          />
         </div>
         <div>
-            <label htmlFor="">Senha</label>
-            <input type="password" {...register("senha", {
-                required: "Por favor informe uma senha"
-            })}/>
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            {...register("email", {
+              required: "Por favor informe seu email",
+            })}
+          />
         </div>
-        
-        
+        <div>
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            placeholder="Digite uma senha"
+            {...register("senha", {
+              required: "Por favor informe uma senha",
+            })}
+          />
+        </div>
+
         {/* campos do endereço */}
         <div>
-            <label htmlFor="">CEP</label>
-            <input type="text" {...register("cep", {
-                required: "Por favor informe seu CEP"
-            })}/>
+          <label htmlFor="">CEP</label>
+          <input
+            type="text"
+            placeholder="Digite seu CEP"
+            {...register("cep", {
+              required: "Por favor informe seu CEP",
+              onBlur : () => buscaCep()
+            })}
+          />
         </div>
         <div>
-            <label htmlFor="">Logradouro</label>
-            <input type="text" {...register("logradouro")} disabled />
+          <label htmlFor="">Logradouro</label>
+          <input type="text" {...register("logradouro")} disabled />
         </div>
         <div>
-            <label htmlFor="">Complemento</label>
-            <input type="text" {...register("complemento")} disabled/>
+          <label htmlFor="">Complemento</label>
+          <input type="text" {...register("complemento")} disabled />
         </div>
         <div>
-            <label htmlFor="">Bairro</label>
-            <input type="text" {...register("bairro")} disabled/>
+          <label htmlFor="">Bairro</label>
+          <input type="text" {...register("bairro")} disabled />
         </div>
         <div>
-            <label htmlFor="">Cidade</label>
-            <input type="text" {...register("cidade")} disabled/>
+          <label htmlFor="">Cidade</label>
+          <input type="text" {...register("cidade")} disabled />
         </div>
         <div>
-            <label htmlFor="">Estado</label>
-            <input type="text" {...register("uf")} disabled/>
+          <label htmlFor="">Estado</label>
+          <input type="text" {...register("uf")} disabled />
         </div>
         <div>
-            <label htmlFor="">Número</label>
-            <input type="text" {...register("numero", {
-                required: "Por favor informe o número da sua residencia"
-            })}/>
+          <label htmlFor="">Número</label>
+          <input
+            type="text"
+            placeholder="Digite o numero da sua residencia"
+            {...register("numero", {
+              required: "Por favor informe o número da sua residencia",
+            })}
+          />
         </div>
       </form>
     </>
