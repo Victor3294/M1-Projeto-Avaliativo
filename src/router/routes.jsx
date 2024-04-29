2331d849c03bffe0b1bce2f1fd234730e6aa4e04
Victor3294 <vblum26@gmail.com>
2024-04-29 17:48:47 -0300

    feat: 🎸 Implementação da rota privada pós login
    A rota privada controla quando a pessoa é permitida a ir para a pagina
    Dashboard, funcionando através da validação do isAutenticado do
    localStorage, que é setado para true após o login, se não estiver true,
    ele redireciona a pessoa para a pagina de login automaticamente

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom"
+import {Navigate, createBrowserRouter} from "react-router-dom"
 import App from "../App"
 import Login from "../pages/Login"
 import CadastroUsuario from "../pages/CadastroUsuario"
@@ -6,18 +6,29 @@ import Dashboard from "../pages/Dashboard"
 import CadastroLocalDeColeta from "../pages/CadastroLocalDeColeta"
 import ListagemLocalDeColeta from "../pages/ListagemLocalDeColeta"
 
+let isAutenticado = JSON.parse(localStorage.getItem("isAutenticado")) || false
+
+const PrivateRoute = ({children}) => {
+    return isAutenticado ? children : <Navigate to="/login"/>
+}
+
+
 const routes = createBrowserRouter([
     {
         path: "/login",
         element: <Login />
     },
     {
-        path: "/cadastroUsuario",
+        path: "/usuario/cadastro",
         element: <CadastroUsuario />
     },
     {
         path : "/",
-        element: <App />,
+        element: (
+            <PrivateRoute>
+                <App/>
+            </PrivateRoute>
+        ),
         children : [
             {
                 path : "/",
