9c7836c7d430b0c7d7a54cd6adbc5ab63fbe6eec
Victor3294 <vblum26@gmail.com>
2024-04-29 16:58:13 -0300

    feat: 🎸 Criação dos inputs da pagina de login
    Inputs criados com o useForm

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,31 @@
-function Login(){
-    return(
-        <></>
-    )
+import { useForm } from "react-hook-form";
+
+function Login() {
+  const {
+    register
+  } = useForm();
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Informe seu email.",
+            })}
+          />
+        </div>
+        <div>
+            <label htmlFor="">Senha</label>
+            <input type="password" 
+            {...register("senha", {
+                required: "Informe sua senha.",
+            })}/>           
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </>
+  );
 }
-export default Login
+export default Login;
