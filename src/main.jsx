ce81ea388c52112003434646f50534de51563a10
Victor3294 <vblum26@gmail.com>
2024-05-01 14:42:14 -0300

    feat: 🎸 Adição do LocalDeColetaContextProvider na main
    Além da implementação desse context, o cadastro de um local de coleta no
    db.json está sendo enviado nesse commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import routes from "./router/routes.jsx"
 import {RouterProvider} from "react-router-dom"
 import './index.css'
 import {UsuariosContextProvider} from "./context/UsuariosContext.jsx"
+import { LocalDeColetaContextProvider } from './context/LocalDeColetaContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UsuariosContextProvider>
-    <RouterProvider router={routes} />
+    <LocalDeColetaContextProvider>
+      <RouterProvider router={routes} />
+    </LocalDeColetaContextProvider>
   </UsuariosContextProvider>
     
 )
