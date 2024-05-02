c3879929da37bebaf26486fe26194a152f4b6688
Victor3294 <vblum26@gmail.com>
2024-05-01 21:16:04 -0300

    feat: 🎸 Conteudo do Card do local de coleta
    Apenas o componente pronto, ele não foi importado em lugar algum ainda

--- src/components/CardLocalColeta/index.jsx
+++ src/components/CardLocalColeta/index.jsx
@@ -1,7 +1,19 @@
-function CardLocalColeta(){
+function CardLocalColeta({localDeColeta}){
     return(
-        <>
-        </>
+        <div>
+            <span>Nome do Local: {localDeColeta.nomeLocal}</span>
+            <span>Descrição do Local: {localDeColeta.descricaoLocal}</span>
+            <span>CEP: {localDeColeta.cep}</span>
+            <span>Logradouro: {localDeColeta.logradouro}</span>
+            <span>Complemento: {localDeColeta.complemento}</span>
+            <span>Bairro: {localDeColeta.bairro}</span>
+            <span>Cidade: {localDeColeta.cidade}</span>
+            <span>Estado: {localDeColeta.uf}</span>
+            <span>Numero: {localDeColeta.numero}</span>
+            <span>Latitude: {localDeColeta.latitude}</span>
+            <span>Longitude: {localDeColeta.longitude}</span>
+            <span>Tipo de Residuo que Coleta: {localDeColeta.tiposResiduos}</span>
+        </div>
     )
 }
 
