30b4130d26fcd914462e3286a87aa2a9350b0a8a
Victor3294 <vblum26@gmail.com>
2024-05-02 18:27:13 -0300

    feat: 🎸 Colocando CardLocalDeColeta na tela de listagem
    Os cards ja vem com as informações tiradas do context

--- src/pages/ListagemLocalDeColeta/index.jsx
+++ src/pages/ListagemLocalDeColeta/index.jsx
@@ -1,9 +1,16 @@
 import Menu from "../../components/Menu"
+import CardLocalColeta from "../../components/CardLocalColeta"
+import { useContext } from "react"
+import { LocalDeColetaContext } from "../../context/LocalDeColetaContext"
 
 function ListagemLocalDeColeta(){
+    const {locaisDeColeta} = useContext(LocalDeColetaContext)
     return (
         <>
             <Menu></Menu>
+            {!!locaisDeColeta && locaisDeColeta.map((localDeColeta) => (
+                <CardLocalColeta key={localDeColeta.id} localDeColeta={localDeColeta}></CardLocalColeta>
+            ))}
         </>
     )
 }
