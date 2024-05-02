a0e7cbc9bf8d6c224b9d2d2a2156b086e3d83b04
Victor3294 <vblum26@gmail.com>
2024-05-02 20:19:00 -0300

    feat: 🎸 Adição da funcionalidade do botão editar
    Função de levar para a  pagina de cadastro levando o id do local de
    coleta junto, que sera usado para conseguir as informações do local de
    coleta que o usuario deseja editar, mas a pagina de cadastro não foi
    atualizada para executar essa função ainda

--- src/pages/ListagemLocalDeColeta/index.jsx
+++ src/pages/ListagemLocalDeColeta/index.jsx
@@ -2,15 +2,16 @@ import Menu from "../../components/Menu"
 import CardLocalColeta from "../../components/CardLocalColeta"
 import { useContext } from "react"
 import { LocalDeColetaContext } from "../../context/LocalDeColetaContext"
-
+import { useNavigate } from "react-router-dom"
 function ListagemLocalDeColeta(){
     const {locaisDeColeta, excluirLocalDeColeta} = useContext(LocalDeColetaContext)
+    const navigate = useNavigate()
     return (
         <>
             <Menu></Menu>
             {!!locaisDeColeta && locaisDeColeta.map((localDeColeta) => (
                 <CardLocalColeta key={localDeColeta.id} localDeColeta={localDeColeta}>
-                    <button>Editar</button>
+                    <button onClick={() => {navigate(`/cadastroLocalDeColeta/${localDeColeta.id}`, {replace: true})}}>Editar</button>
                     <button onClick={() => {excluirLocalDeColeta(localDeColeta.id)}}>Excluir</button>
                 </CardLocalColeta>
             ))}
