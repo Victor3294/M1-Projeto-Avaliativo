acd0f5c8e217ce4cf2bab01f79f7c31cc6bbcb64
Victor3294 <vblum26@gmail.com>
2024-05-04 23:32:16 -0300

    style: 💄 Estilização simples da pagina de listagem
    Estilização dos botões do card e posicionamento dos cards na pagina

--- src/pages/ListagemLocalDeColeta/index.jsx
+++ src/pages/ListagemLocalDeColeta/index.jsx
@@ -3,18 +3,21 @@ import CardLocalColeta from "../../components/CardLocalColeta"
 import { useContext } from "react"
 import { LocalDeColetaContext } from "../../context/LocalDeColetaContext"
 import { useNavigate } from "react-router-dom"
+import style from "./style.module.css"
 function ListagemLocalDeColeta(){
     const {locaisDeColeta, excluirLocalDeColeta} = useContext(LocalDeColetaContext)
     const navigate = useNavigate()
     return (
         <>
             <Menu></Menu>
-            {!!locaisDeColeta && locaisDeColeta.map((localDeColeta) => (
-                <CardLocalColeta key={localDeColeta.id} localDeColeta={localDeColeta}>
-                    <button onClick={() => {navigate(`/cadastroLocalDeColeta/${localDeColeta.id}`, {replace: true})}}>Editar</button>
-                    <button onClick={() => {excluirLocalDeColeta(localDeColeta.id)}}>Excluir</button>
-                </CardLocalColeta>
-            ))}
+            <div className={style.containerCardLocalDeColeta}>
+                {!!locaisDeColeta && locaisDeColeta.map((localDeColeta) => (
+                    <CardLocalColeta key={localDeColeta.id} localDeColeta={localDeColeta}>
+                        <button className={style.button} onClick={() => {navigate(`/cadastroLocalDeColeta/${localDeColeta.id}`, {replace: true})}}>Editar</button>
+                        <button className={style.button} onClick={() => {excluirLocalDeColeta(localDeColeta.id)}}>Excluir</button>
+                    </CardLocalColeta>
+                ))}
+            </div>
         </>
     )
 }
